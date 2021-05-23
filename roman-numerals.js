d1bd764bd85679b57614239b30ab4c0bb7affae2
bertman12 <eddydaniel94@gmail.com>
2021-05-22 20:39:00 -0700

    roman-numerals webpage

--- roman-numerals.js
+++ roman-numerals.js
@@ -55,95 +55,101 @@ THE ALGORITHM WOULD START FROM THE BEGINNING OF THE ARRAY AND WORK TO THE END WH
     
     */
 
-let inputNumber = 9;
-let computedInputNumber = [];
-let computedRomanNumerals = [];
-let convertedNumber = "";
 
-const ROMAN_NUMERALS = [
-    ['I', 'V'],    //1 - 9
-    ['X', 'L'],    //10 - 99
-    ['C', 'D'],    //100 - 999
-    ['M', 'v'],    //1000 - 9999
-    ['x']          //10000
-];
+function convertNumber (){
 
-function computeInput(number) {
-    let remainder = null;
-    const localArray = [];
+    let inputNumber = inputField.value;
+    let computedInputNumber = [];
+    let computedRomanNumerals = [];
+    let convertedNumber = "";
 
-    if(number <= 4000){ //4000 is chosen for the freecodecamp.org test cases but can be scaled up if necessary
-        localArray.unshift(Math.floor(number/1000));//values are 1 - 4. This range can change based on your conditional
-        remainder = number % 1000; //remainder is never greater than 1000
-        localArray.unshift(Math.floor(remainder/100)); //values are 1 - 10
-        remainder = number % 100; //remainder is never greater than 100
-        localArray.unshift(Math.floor(remainder/10)); //values are 1 - 9
-        remainder = number % 10; //remainder is never greater than 10
-        localArray.unshift(Math.floor(remainder)); //values are 1 - 9
-    }
-    return localArray;
-}
+    const ROMAN_NUMERALS = [
+        ['I', 'V'],    //1 - 9
+        ['X', 'L'],    //10 - 99
+        ['C', 'D'],    //100 - 999
+        ['M', 'v'],    //1000 - 9999
+        ['x']          //10000
+    ];
 
-function createRomanNumerals (numberArray) {
-    const localArray = [];
-    
-    //make decisions for which roman numeral to use
-    //we are going to interact with the roman numerals array with respect to the current index of our number array
-    //we will use i for tracking our index and matching respectively with the roman numeral array
-
-    for(let i = 0; i < numberArray.length; i++){
+    function computeInput(number) {
         
-        if(i != 3){
-            switch(numberArray[i]){
-                case 0: //temporarily used to insert empty spaces to show 0 index value
-                    //localArray.unshift('_');
-                break;
-                case 4: //uses a combination of characters from current sub array
-                    localArray.unshift(ROMAN_NUMERALS[i][1]);
-                    localArray.unshift(ROMAN_NUMERALS[i][0]);
-                break;
-                case 5: //uses single character of sub array
-                    localArray.unshift(ROMAN_NUMERALS[i][1]); //this will add the largest roman numeral in the apprpriate bracket
-                break;
-                case 9: //uses a combination of characters from current sub array and borrows a character from the next sub array
-                    localArray.unshift(ROMAN_NUMERALS[i + 1][0]);
-                    localArray.unshift(ROMAN_NUMERALS[i][0]);
-                break;
-                case 10: //borrows a character from the next sub array
-                    localArray.unshift(ROMAN_NUMERALS[i + 1][0]);
-                break;
+        let remainder = null;
+        const localArray = [];
 
-                //default will handle a number range whose algorithm is indifferent
-                //needs to handle case ragnges 1, 2, 3 AND 6, 7, 8
-                //each range requires 3 loops
-                default:
-                    //values 1, 2, 3
-                    if(numberArray[i] < 4){
-                        for(let x = 0; x < numberArray[i]; x++){
-                            localArray.unshift(ROMAN_NUMERALS[i][0]);
-                        }
-                    }
-                    //values 6, 7, 8
-                    else{ 
-                        for(let x = 0; x < numberArray[i] - 5; x++){
-                            localArray.unshift(ROMAN_NUMERALS[i][0]);
-                        }
+        if(number <= 4000){ //4000 is chosen for the freecodecamp.org test cases but can be scaled up if necessary
+            localArray.unshift(Math.floor(number/1000));//values are 1 - 4. This range can change based on your conditional
+            remainder = number % 1000; //remainder is never greater than 1000
+            localArray.unshift(Math.floor(remainder/100)); //values are 1 - 10
+            remainder = number % 100; //remainder is never greater than 100
+            localArray.unshift(Math.floor(remainder/10)); //values are 1 - 9
+            remainder = number % 10; //remainder is never greater than 10
+            localArray.unshift(Math.floor(remainder)); //values are 1 - 9
+        }
+        return localArray;
+    }
+
+    function createRomanNumerals (numberArray) {
+        const localArray = [];
+        
+        //make decisions for which roman numeral to use
+        //we are going to interact with the roman numerals array with respect to the current index of our number array
+        //we will use i for tracking our index and matching respectively with the roman numeral array
+
+        for(let i = 0; i < numberArray.length; i++){
+            
+            if(i != 3){
+                switch(numberArray[i]){
+                    case 0: //temporarily used to insert empty spaces to show 0 index value
+                        //localArray.unshift('_');
+                    break;
+                    case 4: //uses a combination of characters from current sub array
                         localArray.unshift(ROMAN_NUMERALS[i][1]);
-                    }
-            }//end switch statement
-        }//end if statement
-        else if(i = 3){
-            for(let x = 0; x < numberArray[i]; x++){
-                localArray.unshift(ROMAN_NUMERALS[i][0]);
+                        localArray.unshift(ROMAN_NUMERALS[i][0]);
+                    break;
+                    case 5: //uses single character of sub array
+                        localArray.unshift(ROMAN_NUMERALS[i][1]); //this will add the largest roman numeral in the apprpriate bracket
+                    break;
+                    case 9: //uses a combination of characters from current sub array and borrows a character from the next sub array
+                        localArray.unshift(ROMAN_NUMERALS[i + 1][0]);
+                        localArray.unshift(ROMAN_NUMERALS[i][0]);
+                    break;
+                    case 10: //borrows a character from the next sub array
+                        localArray.unshift(ROMAN_NUMERALS[i + 1][0]);
+                    break;
+
+                    //default will handle a number range whose algorithm is indifferent
+                    //needs to handle case ragnges 1, 2, 3 AND 6, 7, 8
+                    //each range requires 3 loops
+                    default:
+                        //values 1, 2, 3
+                        if(numberArray[i] < 4){
+                            for(let x = 0; x < numberArray[i]; x++){
+                                localArray.unshift(ROMAN_NUMERALS[i][0]);
+                            }
+                        }
+                        //values 6, 7, 8
+                        else{ 
+                            for(let x = 0; x < numberArray[i] - 5; x++){
+                                localArray.unshift(ROMAN_NUMERALS[i][0]);
+                            }
+                            localArray.unshift(ROMAN_NUMERALS[i][1]);
+                        }
+                }//end switch statement
+            }//end if statement
+            else if(i = 3){
+                for(let x = 0; x < numberArray[i]; x++){
+                    localArray.unshift(ROMAN_NUMERALS[i][0]);
+                }
             }
         }
+        return localArray;
     }
-    return localArray;
-}
 
-computedInputNumber = computeInput(inputNumber);
-computedRomanNumerals = createRomanNumerals(computedInputNumber);
-convertedNumber = computedRomanNumerals.join('');
-console.log(computedInputNumber);
-console.log(computedRomanNumerals);
-console.log(convertedNumber);
+    computedInputNumber = computeInput(inputNumber);
+    computedRomanNumerals = createRomanNumerals(computedInputNumber);
+    convertedNumber = computedRomanNumerals.join('');
+    // console.log(computedInputNumber);
+    // console.log(computedRomanNumerals);
+    // console.log(convertedNumber);
+    outputTextArea.innerHTML = convertedNumber;
+}
